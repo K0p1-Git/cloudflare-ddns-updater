4318f9a95b82676667703cfeaf9d79453ca48f53
Joel Gray <joeltgray@gmail.com>
2024-02-19 17:47:02 +0000

    Enable update multiple domains with same IP

--- cloudflare-template.sh
+++ cloudflare-template.sh
@@ -5,7 +5,7 @@ auth_email=""                                       # The email used to login 'h
 auth_method="token"                                 # Set to "global" for Global API Key or "token" for Scoped API Token
 auth_key=""                                         # Your API Token or Global API Key
 zone_identifier=""                                  # Can be found in the "Overview" tab of your domain
-record_name=""                                      # Which record you want to be synced
+record_names=()                                     # Which records you want to be synced Format: ("domain.com" "mydomain.net")
 ttl="3600"                                          # Set the DNS TTL (seconds)
 proxy="false"                                       # Set the proxy to true or false
 sitename=""                                         # Title of site "Example Site"
@@ -47,42 +47,45 @@ fi
 ###########################################
 
 logger "DDNS Updater: Check Initiated"
-record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A&name=$record_name" \
-                      -H "X-Auth-Email: $auth_email" \
-                      -H "$auth_header $auth_key" \
-                      -H "Content-Type: application/json")
+for record_name in "${record_names[@]}"; do
+	record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?type=A&name=$record_name" \
+        	              -H "X-Auth-Email: $auth_email" \
+                	      -H "$auth_header $auth_key" \
+                     	      -H "Content-Type: application/json")
 
-###########################################
-## Check if the domain has an A record
-###########################################
-if [[ $record == *"\"count\":0"* ]]; then
-  logger -s "DDNS Updater: Record does not exist, perhaps create one first? (${ip} for ${record_name})"
-  exit 1
-fi
+	###########################################
+	## Check if the domain has an A record
+	###########################################
+	if [[ $record == *"\"count\":0"* ]]; then
+  		logger -s "DDNS Updater: Record does not exist, perhaps create one first? (${ip} for ${record_name})"
+  		exit 1
+	fi
 
-###########################################
-## Get existing IP
-###########################################
-old_ip=$(echo "$record" | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
-# Compare if they're the same
-if [[ $ip == $old_ip ]]; then
-  logger "DDNS Updater: IP ($ip) for ${record_name} has not changed."
-  exit 0
-fi
+	###########################################
+	## Get existing IP
+	###########################################
+	old_ip=$(echo "$record" | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
+	# Compare if they're the same
+	if [[ $ip == $old_ip ]]; then
+  		logger "DDNS Updater: IP ($ip) for ${record_name} has not changed."
+  		exit 0
+	fi
 
-###########################################
-## Set the record identifier from result
-###########################################
-record_identifier=$(echo "$record" | sed -E 's/.*"id":"([A-Za-z0-9_]+)".*/\1/')
+	###########################################
+	## Set the record identifier from result
+	###########################################
+	record_identifier=$(echo "$record" | sed -E 's/.*"id":"(\w+)".*/\1/')
 
-###########################################
-## Change the IP@Cloudflare using the API
-###########################################
-update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
-                     -H "X-Auth-Email: $auth_email" \
-                     -H "$auth_header $auth_key" \
-                     -H "Content-Type: application/json" \
-                     --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":${proxy}}")
+	###########################################
+	## Change the IP@Cloudflare using the API
+	###########################################
+	update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
+	                     -H "X-Auth-Email: $auth_email" \
+        	             -H "$auth_header $auth_key" \
+                	     -H "Content-Type: application/json" \
+	                     --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":${proxy}}")
+
+done
 
 ###########################################
 ## Report the status
